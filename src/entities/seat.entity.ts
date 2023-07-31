58fa68c82bc618ead7e351710d51d1199f4e8d69
jangdu <jjd0324@gmail.com>
2023-07-31 11:46:19 +0900

    docs: entities에 swagger 정보 추가

--- src/entities/seat.entity.ts
+++ src/entities/seat.entity.ts
@@ -13,21 +13,47 @@ import {
 } from 'typeorm';
 import { ShowEntity } from './show.entity';
 import { ReservationEntity } from './reservation.entity';
+import { IsNumber, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('seat')
 export class SeatEntity extends BaseEntity {
   @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
   id: number;
 
+  @IsNumber()
+  @ApiProperty({
+    example: '4',
+    description: '공연 좌석',
+    required: true,
+  })
   @Column({ type: 'int', name: 'seatNumber' })
   seatNumber: number;
 
+  @IsString()
+  @ApiProperty({
+    example: 'A',
+    description: '공연 좌석 등급',
+    required: true,
+  })
   @Column({ type: 'varchar', length: 10, name: 'grade' })
   grade: string;
 
+  @IsString()
+  @ApiProperty({
+    example: '10000',
+    description: '공연 가격',
+    required: true,
+  })
   @Column({ type: 'int', name: 'price' })
   price: number;
 
+  @IsString()
+  @ApiProperty({
+    example: '1',
+    description: '공연장 id',
+    required: true,
+  })
   @Column()
   showId: number;
 
--- src/entities/show.entity.ts
+++ src/entities/show.entity.ts
@@ -11,21 +11,47 @@ import {
 } from 'typeorm';
 import { ReservationEntity } from './reservation.entity';
 import { SeatEntity } from './seat.entity';
+import { IsDate, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity('show')
 export class ShowEntity extends BaseEntity {
   @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
   id: number;
 
+  @IsString()
+  @ApiProperty({
+    example: '장두혁의 뮤지컬 1',
+    description: '공연 이름',
+    required: true,
+  })
   @Column({ type: 'varchar', length: 30, name: 'title' })
   title: string;
 
+  @IsString()
+  @ApiProperty({
+    example: '예술의 전당',
+    description: '공연장 위치',
+    required: true,
+  })
   @Column({ type: 'varchar', length: 50, name: 'location', select: false })
   location: string;
 
+  @IsString()
+  @ApiProperty({
+    example: '뮤지컬',
+    description: '공연 카테고리',
+    required: true,
+  })
   @Column({ type: 'varchar', length: 30, name: 'category' })
   category: string;
 
+  @IsDate()
+  @ApiProperty({
+    example: '2023-08-20 14:00:00',
+    description: '공연 날짜 및 시간',
+    required: true,
+  })
   @Column({ type: 'datetime', name: 'date' })
   date: Date;
 
